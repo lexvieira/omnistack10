546289b800ae1633645c9182517bdc9bbfe9efab
lexvieira <lexvieira.info@gmail.com>
2021-01-20 20:58:26 +0700

    Update and Tests with React Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+const { Router, request, response } = require('express');
 const DevController = require('./controllers/DevController');
 const SearchController = require('./controllers/SearchController');
 
@@ -7,6 +7,14 @@ const SearchController = require('./controllers/SearchController');
 const routes = Router();
 
 //Routes in plural
+//Start Route
+routes.get('/', (request, response) => {
+    console.log(request.query);
+    return response.send('Api Access, access the main page on <a href="http://localhost">http://localhost</a>');
+    //return response.json('Api Access Json');
+});
+
+
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 
