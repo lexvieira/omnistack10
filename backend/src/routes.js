4d46e028e49aeda8def912f108b99405fa3c3544
lexvieira <lexvieira.info@gmail.com>
2021-03-18 19:18:48 +0700

    Update Routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -14,10 +14,8 @@ routes.get('/', (request, response) => {
     //return response.json('Api Access Json');
 });
 
-
 routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
-
-routes.get('/search', SearchController.index)
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
